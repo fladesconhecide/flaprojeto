959edc01c536de17aed1d9a4030f31080ae6ad75
fladesconhecide <132388217+fladesconhecide@users.noreply.github.com>
2023-05-02 19:12:42 +0000

    flacommmit

---
+++ flagenda/flagenda.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"flagenda.h"
+
+#define MAX_CHAR 100
+
+struct flacontato{
+    char flanome[MAX_CHAR];
+    int idade;
+    int tel;
+    char email[MAX_CHAR];
+};
+
+struct flagenda{
+    char nome[MAX_CHAR];
+    int cod;
+    int num;
+    flacontatos contato;
+};
+
+struct flano{
+    flacontatos dado;
+    no *prox;
+};
+
+struct lista{
+    no * inicio;
+};
+
+void criar_inicio(flalista *lista, flacontatos dado){
+    no * novo = (no*) malloc (sizeof(no));
+    novo->dado = dado;
+    novo->prox = lista->inicio;
+    lista->inicio = novo;
+}
+
+
+
+void fladiciona(){
+
+}
---
+++ flagenda/flagenda.h
@@ -0,0 +1,12 @@
+#ifndef FLAGENDA_H_INCLUDED
+#define FLAGENDA_H_INCLUDED
+
+typedef struct flacontato flacontatos;
+
+typedef struct flagenda flaagenda;
+
+typedef struct flano no;
+
+typedef struct lista flalista;
+
+#endif
